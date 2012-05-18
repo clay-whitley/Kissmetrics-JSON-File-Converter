9dc3d95e9c1380c0f063a4eddf6d4149a2255f22
Eddy Kim <eddykim87@gmail.com>
2012-05-18 10:28:04 -0700

    initial

---
+++ convert.rb
@@ -0,0 +1,32 @@
+## Eddy Kim
+## convert.rb
+## Converts KissMetrics JSON files into a single csv file
+
+
+require 'json'
+require 'date'
+require 'csv'
+
+if File.exists?("data.csv")
+  puts 'Error: data.csv already exists, please rename or remove it'
+else
+
+puts 'Converting *.json files into data.csv...'
+
+Dir.glob('./*.json') do |file|
+  json = ""
+  begin
+    json = File.read(file)
+    puts "Parsing [" + file + "]"
+    json.split("\n").each do |data|
+      temp = JSON.parse(data)
+      CSV.open("data.csv", "a") do |csv|
+        csv << [ temp['_n'], temp["_p"], DateTime.strptime(temp['_t'].to_s,'%s')]
+        puts temp['_n']
+      end
+    end
+  rescue
+    puts "Error: Parsing [#{file}]"
+  end
+end
+end
